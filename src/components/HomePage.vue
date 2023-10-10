b863f4bd2b63d9a9604b69eb8705b1310be4d570
smorenolasa1 <smoreno.ieu2021@student.ie.edu>
2023-10-11 01:41:34 +0200

    check program

---
+++ src/components/HomePage.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h1>HomePage</h1>
+        <p>welcone to this bank.</p>
+        <router-link to="/skull">
+      <button type="button" class="btn btn-secondary">Test Connection</button>
+    </router-link>
+    <br /><br />
+    <router-link to="/accounts">
+      <button type="button" class="btn btn-secondary">View Accounts</button>
+    </router-link>
+    </div>
+</template>
---
+++ src/router/index.js
@@ -0,0 +1,33 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import HomePage from '../components/HomePage.vue'
+import Skull from '../components/Skull.vue'
+import AppAccounts from '../components/AppAccounts.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'HomePage',
+    component: HomePage
+  },
+  {
+    path: '/skull',
+    name: 'Skull',
+    component: Skull
+  },
+  {
+    path: '/accounts',
+    name: 'AppAccounts',
+    component: AppAccounts
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
